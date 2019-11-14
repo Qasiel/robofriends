ed6f00175976aed02ac16e803b77086be4ce8d9a
Joe Smith <joe@sitejacket.co.uk>
2019-11-14 17:56:44 +0000

    Updates

---
+++ precache-manifest.23dfd348f159fb37fd8eed00cfce4b6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7e08d23d399b8863e986e83c3fb5bf",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "04130c833c2154f9d603",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1533b749bd367a9e79a1",
+    "url": "/robofriends/static/css/main.482cb1b4.chunk.css"
+  },
+  {
+    "revision": "04130c833c2154f9d603",
+    "url": "/robofriends/static/js/2.ada142a8.chunk.js"
+  },
+  {
+    "revision": "1533b749bd367a9e79a1",
+    "url": "/robofriends/static/js/main.4e8fb242.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
